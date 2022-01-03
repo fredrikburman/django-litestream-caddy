85b9c3351a177abef00219c8c20758e7093331e1
fredrik burman <fredrik.burman@gmail.com>
2022-01-03 08:08:55 +0100

    lgtm

---
+++ web/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# if the db file doesn't exist we get it from the REPLICA_URL
+[ ! -f $DB_FILE ] && litestream restore -v -if-replica-exists -o $DB_FILE "${REPLICA_URL}" \
+  ; python manage.py flush --no-input \
+  ; python manage.py migrate \
+  ; python manage.py collectstatic --no-input --clear \
+  ; litestream replicate -exec "python manage.py runserver 0.0.0.0:8000" $DB_FILE "${REPLICA_URL}"
+
